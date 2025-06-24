0569de6ebbc05a95a0d2f6978aa1252c25921f99
User 01 <75183951+berrami5@users.noreply.github.com>
2025-06-24 11:12:20 +0100

    Buttons - pull up and pull down

--- sketch_jun3a/sketch_jun3a.ino
+++ sketch_jun3a/sketch_jun3a.ino
@@ -1,28 +1,15 @@
-int banary (int n, int index) {
-    int res[4];
-    int counter = 3;
-    while (n > 0) {
-        int c = n % 2;
-        n /= 2;
-        res[counter--] = c;
-    }
-    return res[index];
-}
+int btn_pulldown = 13;
 void setup()
 {
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(13, INPUT);
+  pinMode(12, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-   for (int i = 0 ; i < 16 ; i++) {
-    digitalWrite(13 , banary(i , 0));
-    digitalWrite(12 , banary(i , 1));
-    digitalWrite(11 , banary(i , 2));
-    digitalWrite(10 , banary(i , 3));
-    delay(500);
-   }
+      Serial.print(digitalRead(13));
+      Serial.print(" ");
+      Serial.print(digitalRead(12));
+      Serial.print("\n");
+      delay(500);
 }
