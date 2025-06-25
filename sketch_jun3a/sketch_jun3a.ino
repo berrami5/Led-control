c2148b6906501ea5d8fe9588144b031f15f35957
User 01 <75183951+berrami5@users.noreply.github.com>
2025-06-25 13:09:05 +0100

    motion detection

--- sketch_jun3a/sketch_jun3a.ino
+++ sketch_jun3a/sketch_jun3a.ino
@@ -1,18 +1,11 @@
-int prev = 1;
-int current = 1;
-bool v2 = false;
 void setup() {
   pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  
-      v2 = digitalRead(2);
-      Serial.print(current);
-      if (v2 == 1){prev = current++;}
-      if (current % 2 == 0) digitalWrite(13, 1);
-      if (current % 2 != 0) digitalWrite(13, 0);
+      Serial.println( digitalRead(2));
+      digitalWrite(13,1);
       delay(500);
 }
